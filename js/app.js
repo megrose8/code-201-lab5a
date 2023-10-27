8dfd377b0cadeaa17271690aef42f0c783a5ec26
MEGAN BREEN <MBREEN@GICW.ORG>
2023-10-27 15:06:19 -0700

    sum arrray solved

--- js/app.js
+++ js/app.js
@@ -78,12 +78,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+  let total = 0;
+  for (let i = 0; i < sumArr.length; i++) {
+    total = sum(total, sumArr[i])[0];
+  }
 
+  const sumMessage = `${sumArr.join(',')} was passed in as an array of numbers, and ${total} is their sum.`;
+  return [total, sumMessage];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
